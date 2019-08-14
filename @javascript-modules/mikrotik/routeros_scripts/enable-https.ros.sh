b90985cb3aab7e7965134dd6ab78bf6a393302cc
codepad <codepad@bp-devel>
2019-08-14 19:15:43 +0200

    2.0.110.0

--- @javascript-modules/mikrotik/routeros_scripts/enable-https.ros.sh
+++ @javascript-modules/mikrotik/routeros_scripts/enable-https.ros.sh
@@ -1,6 +1,8 @@
 ## Enable HTTPS
 ## based on https://www.medo64.com/2016/11/enabling-https-on-mikrotik/
 
+## /tool fetch url="https://raw.githubusercontent.com/LaKing/boilerplate/master/%40javascript-modules/mikrotik/routeros_scripts/enable-https.ros.sh" mode=https
+
 /certificate
 add name=root-cert common-name=MyRouter days-valid=3650 key-usage=key-cert-sign,crl-sign
 sign root-cert
