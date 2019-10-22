6a514ad6e7e08af741a76bdcd0b63a53cc53094c
codepad <codepad@bp-devel>
2019-10-22 07:39:17 +0200

    2.0.121.9

--- publish.sh
+++ publish.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+version="$(cat version)"
+
 echo "git add ."
 git add .
-echo "git commit -m codepad"
-git commit -m "$(cat version)"
+echo "git commit -m $version"
+git commit -m "$version"
 echo "git push"
 git push
 echo "OK"
